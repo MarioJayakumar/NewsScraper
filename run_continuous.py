380d3d71cc7adc2eb341e1d970f34b543a47ade5
Mario Jayakumar <mario.jayakumar@gmail.com>
2021-05-04 00:09:14 -0400

    continuously running spiders ready

--- run_continuous.py
+++ run_continuous.py
@@ -5,7 +5,9 @@ import subprocess
 def run_all_job():
     subprocess.call(["python3", "scraper.py"], cwd="newsscraper/")
 
-schedule.every().day.at("10:00").do(run_all_job)
-#schedule.every().day.at("22:00").do(run_all_job)
+schedule.every().day.at("10:00").until("11:30").do(run_all_job)
+schedule.every().day.at("22:00").until("23:30").do(run_all_job)
 
-schedule.run_all()
+while True:
+    schedule.run_pending()
+    time.sleep(1)
